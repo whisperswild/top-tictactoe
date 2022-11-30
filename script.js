ea2e242a40c4318c3e3a4a13d02d76dbcdda2a96
Chris <Whisperswild@gmail.com>
2022-11-30 09:10:09 -0600

    Build logic for win, tie, no-win conditions (continue)

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //notes:
 //Build player array then convert to string at each step to check for a winner
 
-function player(name, isAi, score, scoreArray, token, turn, winner){
-    this.name;
-    this.isAi;
-    this.scoreArray;
-    this.score; 
-    this.token; 
-    this.turn; 
-    this.winner;
+function Player(name, isAi, scoreArray, score, token, turn, winner){
+    this.name = name;
+    this.isAi = isAi;
+    this.scoreArray = scoreArray;
+    this.score = score;
+    this.token = token;
+    this.turn = turn;
+    this.winner = false;
 
-    this.playerScore = function(){return console.log(this.score)};
-    this.playerName = function(){return console.log(this.name)},
-    this.convertScore = function(){
+    this.playerScore = () => {return console.log(this.score)};
+    this.playerName = () => {return console.log(this.name)};
+    this.convertScore = () => {
        return this.score = this.scoreArray.join("");
-    };
-    this.checkForWin = function(){
+    },
+    this.checkForWin = () => {
+        this.convertScore();
         const WIN_ARRAY = [
             // Rows
             "111000000", 
@@ -31,46 +32,51 @@ function player(name, isAi, score, scoreArray, token, turn, winner){
         ]
     
         for (let i of WIN_ARRAY){
-            if (i === this.scoreArray.join("")){
-                console.log(`Win: ${this.scoreArray.join("")} and WIN_ARRAY: ${i}`);
-            }else if (this.scoreArray.join("") === "111111111"){
-                console.log(`loser: ${this.name}`);
-            } 
+            if (i === this.score){
+                return this.winner = true
+            }else if (this.score === "111111111"){
+                //Tie game..set to null, no winner
+                return this.winner = null;
+            }else{
+                //No winner yet
+                this.winner = false;
+            }
         }
     }
 }
 
-const playerTurn = {
-    
-}
 
 
-const player1 = new player();
+const player1 = new Player();
 player1.name = "p1";
 player1.token = "X";
-player1.scoreArray = [1,0,0,1,0,0,1,0,0];
+player1.scoreArray = [1,0,0,1,0,0,1,0,1];
 player1.isAi = false;
 player1.turn = true;
 player1.checkForWin();
 
-const player2 = new player();
+const player2 = new Player();
 player2.name = "p2";
 player2.token = "O";
-player2.scoreArray = [1,1,1,1,1,1,1,1,1];
+player2.scoreArray = [1,1,1,0,0,0,0,0,1];
 player2.isAi = true;
 player2.turn = false;
-const win = player2.checkForWin.bind(player2)
-console.log(typeof(win));
+player2.checkForWin();
 
 const button = document.querySelector('#btnSubmit');
-
 button.addEventListener('click', e => {
     e.preventDefault();
 
     const exportDiv = document.createElement('div');
     const resultsDiv = document.querySelector('#resultsDiv');
-    
-    exportDiv.textContent = win;
+
+    if (player1.winner){
+        exportDiv.textContent = `Player 1 winner? ${player1.turn}`;
+    }else if (player2.winner){
+        exportDiv.textContent = `Player 2 winner? ${player2.winner}`;
+    }else{
+        exportDiv.textContent = `No winner yet...`;
+    }
 
     resultsDiv.appendChild(exportDiv);
 
