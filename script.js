d1d2f8c29561077dea247100472049538031dbfb
Chris <Whisperswild@gmail.com>
2022-12-01 08:57:08 -0600

    Build function to create grid and grid's event listeners. Build function to create players.

--- script.js
+++ script.js
@@ -1,13 +1,53 @@
-//notes:
-//Build player array then convert to string at each step to check for a winner
+const gameSquares = [];
+const gameSize = 9; //The total number of squares in the grid. 9 for normal tic-tac-toe
+const numPlayers = 2;
+
+function addPlayers(numPlayers){
+    for (let i = 0; i < numPlayers; i++){
+
+        const player = [];
+        player[i] = new Player();
+        player[i].name = `Player-${i}`;
+        player[i].token = "X"; //this could be a random letter of the alphabet or other token
+        player[i].scoreArray = [0,0,0,0,0,0,0,0,0]; //Need to increase the size of this array based on game size.
+                                                    //This would also affect the win patterns
+        player[i].isAi = false;
+        player[i].turn = false;
+    }
+}
+function buildBoard(totalSquares){
+
+    //since we're dynamically creating the grid, the style needs
+    //to reflect that the grid can be different sizes
+
+    for (let i = 0; i < totalSquares; i++){
+        gameSquares[i] = document.getElementById(`square-${i}`);
+        gameSquares[i].addEventListener('click', e =>{
+            gameTriggers(e.target.id);
+        })
+    }
+}
+buildBoard(gameSize);
+addPlayers(numPlayers);
+
+function gameTriggers(clickedSquare){
+            //the gamesquare event listener. Add an X or O here if clicked. 
+            //if the div already has an X or O, do nothing. Check against textcontent
+            //Set this up so the click triggers a function then do the work in the function
+
+            //check whose turn it is, add that player's token then set their turn to false and the next player's turn to true
+
+}
+
 
 function Player(name, isAi, scoreArray, score, token, turn, winner){
+    //remove 'winner' from the methods list later?
     this.name = name;
     this.isAi = isAi;
     this.scoreArray = scoreArray;
     this.score = score;
     this.token = token;
-    this.turn = turn;
+    this.turn = turn; //bool
     this.winner = false;
 
     this.playerScore = () => {return console.log(this.score)};
